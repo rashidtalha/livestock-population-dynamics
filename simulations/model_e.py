2ef93f3680839a5316701ee62b3c1047e41096ac
rashidtalha <128423607+rashidtalha@users.noreply.github.com>
2024-06-12 14:13:10 +0500

    Simply the Goat and Family classes by removing methods. Minor fixes to the monthly evolution method. Minor improvement to the life expectancy method.

--- simulations/model_e.py
+++ simulations/model_e.py
@@ -16,6 +16,7 @@ prob_female = 0.5
 prob_preg = 0.9
 prob_kids = [0.4, 0.3, 0.2, 0.1]
 
+
 def estimated_life_expectency(female, current_age):
     val = 0
     if female:
@@ -30,29 +31,76 @@ def estimated_life_expectency(female, current_age):
             else:
                 val = rng.integers(tau_start+tau_preg, tau_end)
         if (tau_end <= current_age) or (tau_end == val):
-            val = tau_f
+            # val = tau_f
+            val = rng.integers(tau_end, tau_f)
     else:
-        val = tau_m
+        # val = tau_m
+        val = rng.integers(0, tau_m)
     return val
+
 
 class Society():
     def __init__(self, count=1, starting_age=0):
         self.families = []
+        self.counters = {"inactive_familes":0}
 
         for _ in range(count):
-            f = Family()
+            f = Family(True)
             max_age = estimated_life_expectency(True, starting_age)
-            f.add_goat(female=True, age=starting_age, life=max_age)
+            f.goats.append(Goat(True, starting_age, max_age, False))
             self.families.append(f)
 
-    def evolve_month(self):
-        self.families = [f for f in self.families if f.active]
-        for f in self.families:
-            root_goat = f.step()
-            if root_goat is not None:
-                f = Family()
-                f.add_goat(female=True, age=tau_start, life=root_goat)
+    def next_month(self, count=1):
+        for _ in range(count):
+            new_families = []
+            for f in self.families:
+                for g in f.goats:
+                    ## New family dynamics
+                    if g.starts_new and g.age >= tau_start:
+                        g.alive = False
+                        new_families.append((g.age, g.life))
+                        continue
+
+                    ## Birthing dynamics
+                    if g.preg_duration == tau_preg:
+                        g.preg = False
+                        g.preg_duration = 0
+                        g.last_preg_end = g.age
+                        kids = rng.choice(np.arange(len(prob_kids))+1, p=prob_kids)
+                        
+                        for k in range(kids):
+                            is_female = True if rng.uniform(0,1) < prob_female else False
+                            max_age = estimated_life_expectency(is_female, 0)
+                            starts_new = is_female and (not f.first_female_born) and (max_age > tau_start)
+                            if starts_new:
+                                f.first_female_born = True
+                            f.goats.append(Goat(is_female, 0, max_age, starts_new))
+                        continue
+
+                    ## Pregnancy dynamics
+                    if g.preg:
+                        g.preg_duration += 1
+                    else:
+                        if g.female and g.age >= tau_start and g.age < tau_end and (g.last_preg_end is None or (g.age - g.last_preg_end) >= tau_gap):
+                            g.preg = True if rng.uniform(0,1) < prob_preg else False
+                            g.preg_duration += 1
+
+                    ## Death dynamics
+                    g.age += 1
+                    g.alive = g.age < g.life
+
+                f.goats = [g for g in f.goats if g.alive]
+                f.active = len(f.goats) > 0
+
+            for j in new_families:
+                f = Family(True)
+                f.goats.append(Goat(True, j[0], j[1], False))
                 self.families.append(f)
+
+            a = len(self.families)
+            self.families = [f for f in self.families if f.active]
+            b = len(self.families)
+            self.counters["inactive_familes"] += max(a-b, 0)
 
     @property
     def alive_goats(self):
@@ -72,75 +120,43 @@ class Society():
     def active_families(self):
         return len([f for f in self.families if f.active])
 
+    @property
+    def inactive_families(self):
+        return self.counters["inactive_familes"]
+
 
 class Family():
-    def __init__(self):
+    def __init__(self, active=False):
+        """
+        Parameters:
+        -----------
+        active (Bool):
+            If this family has any goats or not
+        """
         self.goats = []
-        self.active = len(self.goats) > 0
-        self.pledge_complete = False
+        self.active = active
+        self.first_female_born = False
 
-    def step(self):
-        self.goats = [g for g in self.goats if g.alive]
-        root_goat = None
-        for g in self.goats:
-            ## New family dynamics
-            if g.starts_new and g.age >= tau_start:
-                g.alive = False
-                root_goat = g.life_expectancy
-                continue
-
-            ## Pregnancy dynamics
-            if g.preg:
-                g.preg_duration += 1
-            else:
-                if g.female and g.age >= tau_start and g.age < tau_end and (g.last_preg_end is None or (g.age - g.last_preg_end) >= tau_gap):
-                    g.preg = True if rng.uniform(0,1) < prob_preg else False
-                    # g.preg_duration += 1
-
-            ## Birthing dynamics
-            if g.preg_duration == tau_preg:
-                g.preg = False
-                g.preg_duration = 0
-                g.last_preg_end = g.age
-                kids = rng.choice(np.arange(len(prob_kids))+1, p=prob_kids)
-                
-                for k in range(kids):
-                    is_female = True if rng.uniform(0,1) < prob_female else False
-                    max_age = estimated_life_expectency(is_female, 0)
-                    starts_new = is_female and (not self.pledge_complete) and (max_age > tau_start)
-                    if starts_new:
-                        self.pledge_complete = True
-                    self.add_goat(is_female, 0, max_age, starts_new)
-                continue
-
-            ## Death dynamics
-            g.age += 1
-            g.alive = g.age < g.life_expectancy
-        self.active = len([g for g in self.goats if g.alive]) > 0
-        return root_goat
-
-    def add_goat(self, female, age, life, starts_new=False):
-        self.goats.append(Goat(female, age, life, starts_new))
-        self.active = len(self.goats) > 0
-
-    # moving a goat to start a new family is the same as
-    # killing that goat (without counter increments) and
-    # starting a new family with a goat of the same age
 
 class Goat():
-    def __init__(self, female, age, life_expectancy, starts_new=False):
+    def __init__(self, female, age, life, starts_new=False):
+        """
+        Parameters:
+        -----------
+        female (Bool):
+            The gender of the goat
+        age (int):
+            Current the age of the goat, in months [0 <= age < MAX]
+        life (int):
+            The age at which the goat will die, in months [age <= life < MAX]
+        starts_new (Bool):
+            Whether this goat will start a new family (when it reaches maturity) or not
+        """
         self.female = female
         self.starts_new = starts_new
-
-        self.life_expectancy = life_expectancy
+        self.life = life
         self.age = age
-        self.alive = self.age < self.life_expectancy
-        
+        self.alive = self.age < self.life
         self.preg = False
         self.preg_duration = 0
         self.last_preg_end = None
-        # self.pledge_complete = False
-
-    # def step_age(self):
-    #     self.age += 1
-    #     self.alive = self.age < self.life_expectancy
