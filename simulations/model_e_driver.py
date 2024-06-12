10593058deef8128ff3b8fe97d5ab7744fce87ee
rashidtalha <128423607+rashidtalha@users.noreply.github.com>
2024-06-12 14:14:15 +0500

    Improve the format of the results. Add multiple views

--- simulations/model_e_driver.py
+++ simulations/model_e_driver.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 import model_e as model
 
+### Model Parameters
 # model.tau_m = 60
 # model.tau_f = 144
 # model.tau_start = 24
@@ -15,17 +16,18 @@ import model_e as model
 # model.prob_preg = 0.9
 # model.prob_kids = [0.4, 0.3, 0.2, 0.1]
 
-simulation_duration = 12*5 # in months
+### Simulation Parameters
+simulation_duration = 12*4 # in months
 initial_goats = 10 # in months
-initial_age = 24 # in months
+initial_age = model.tau_start # in months
 
 # ### Simple output
 # s = model.Society(initial_goats, initial_age)
-# print(f"After {0:>2} months: {s.alive_goats}, {sum(s.alive_goats)}, {s.active_families}")
+# print(f"After {0:>2} months: {s.alive_goats}, {sum(s.alive_goats)}, {s.active_families}; {s.families[0].goats[0].preg}")
 
 # for m in range(simulation_duration):
-#     s.evolve_month()
-#     print(f"After {m+1:>2} months: {s.alive_goats}, {sum(s.alive_goats)}, {s.active_families}")
+#     s.next_month()
+#     print(f"After {m+1:>2} months: {s.alive_goats}, {sum(s.alive_goats)}, {s.active_families}; {s.families[0].goats[0].preg}")
 
 # ### Timeline output
 # s = model.Society(initial_goats, initial_age)
@@ -36,7 +38,7 @@ initial_age = 24 # in months
 # data[2,0] = sum(s.alive_goats)
 # data[3,0] = s.active_families
 # for m in range(simulation_duration):
-#     s.evolve_month()
+#     s.next_month()
 #     data[0,m+1] = s.alive_goats[0]
 #     data[1,m+1] = s.alive_goats[1]
 #     data[2,m+1] = sum(s.alive_goats)
@@ -44,34 +46,38 @@ initial_age = 24 # in months
 
 # months = np.arange(simulation_duration+1)
 
-# fig, ax = plt.subplots()
-# ax.plot(months, data[0], label="female goats alive")
-# ax.plot(months, data[1], label="male goats alive")
-# ax.plot(months, data[2], label="total goats alive")
-# # ax.plot(months, data[3], label="total families active")
+# fig, ax = plt.subplots(1, 2, figsize=(9,5), constrained_layout=True)
+# ax[0].plot(months, data[0], label="female goats alive")
+# ax[0].plot(months, data[1], label="male goats alive")
+# ax[0].plot(months, data[2], label="total goats alive")
+# ax[0].set(xlim=(0,simulation_duration), ylim=(0,None))
+# ax[0].set_xlabel("months")
+# ax[0].set_title("Counts")
+# ax[0].legend()
 
-# ax.set(xlim=(0,simulation_duration), ylim=(0,None))
-# ax.legend()
+# ax[1].plot(months, data[3], label="total families active")
+# ax[1].set(xlim=(0,simulation_duration), ylim=(max(0, initial_goats-5),None))
+# ax[1].set_xlabel("months")
+# ax[1].set_title("Total families active")
+
 # plt.show()
 
 ### Distribution output
 runs = 500
-data = np.zeros((4,runs))
+data = np.zeros((5,runs))
 
 for r in range(runs):
     s = model.Society(initial_goats, initial_age)
-    for _ in range(simulation_duration):
-        s.evolve_month()
+    s.next_month(simulation_duration)
 
     data[0,r] = s.alive_goats[0]
     data[1,r] = s.alive_goats[1]
     data[2,r] = sum(s.alive_goats)
     data[3,r] = s.active_families
-
+    data[4,r] = s.inactive_families
 
 norm_dist = lambda x, m, s: np.exp(-0.5 * ((x-m)/s)**2) / (s * np.sqrt(2*np.pi))
-x = np.linspace(0,max(data[0])*2,200)
-# q = np.linspace(0,max(data[2])*2,200)
+x = np.linspace(0, 1.05*max(data[2]), 500)
 
 mu_0 = np.mean(data[0])
 si_0 = np.std(data[0])
@@ -82,9 +88,19 @@ si_1 = np.std(data[1])
 mu_2 = np.mean(data[2])
 si_2 = np.std(data[2])
 
-print(f"F: Mean = {mu_0:.3f}, Std = {si_0:.3f}")
-print(f"M: Mean = {mu_1:.3f}, Std = {si_1:.3f}")
-print(f"T: Mean = {mu_2:.3f}, Std = {si_2:.3f}")
+mu_3 = np.mean(data[3])
+si_3 = np.std(data[3])
+
+mu_4 = np.mean(data[4])
+si_4 = np.std(data[4])
+
+print(f"Initial goats {initial_goats}")
+print(f"Simulating for {simulation_duration} months. (Runs = {runs})")
+print(f"Alive female goats: Mean = {mu_0:.3f}, Std = {si_0:.3f}")
+print(f"Alive male goats: Mean = {mu_1:.3f}, Std = {si_1:.3f}")
+print(f"Alive total goats: Mean = {mu_2:.3f}, Std = {si_2:.3f}")
+print(f"Active families: Mean = {mu_3:.3f}, Std = {si_3:.3f}")
+print(f"Inactive families: Mean = {mu_4:.3f}, Std = {si_4:.3f}, Max = {np.max(data[4])}")
 
 fig, ax = plt.subplots(2, 2, figsize=(10,8), constrained_layout=True)
 ax[0,0].hist(data[0], density=True)
@@ -104,7 +120,7 @@ ax[0,1].set_yticks([])
 ax[1,0].hist(data[2], density=True)
 ax[1,0].plot(x, norm_dist(x, mu_2, si_2))
 ax[1,0].axvline(mu_2, c="k")
-ax[1,0].set(xlim=( 0.95*min(data[2]), 1.05*max(data[2] )))
+ax[1,0].set(xlim=( 0.95*min(data[2]), 1.05*max(data[2])))
 ax[1,0].set_title("All goats alive")
 ax[1,0].set_yticks([])
 
